ff9fbf6f59453b5bfb7779085a8c4c1ae03a948d
Mario Moreno <mario_morenor@hotmail.com>
2020-08-29 16:55:51 -0500

    Terminado

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,7 +6,7 @@ require('bootstrap');
 
 window.axios = require('axios').default;
 // TODO CAMBIAR EL DOMINIO
-axios.defaults.baseURL = 'http://nixon.test/api';
+axios.defaults.baseURL = 'https://nixon.test/api';
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
