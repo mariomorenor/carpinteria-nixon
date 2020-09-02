e4684aed3c8746928bae414f069dbd4a77158da9
Mario Moreno <mario_morenor@hotmail.com>
2020-09-02 12:34:28 -0500

    Terminado

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,7 +6,8 @@ require('bootstrap');
 
 window.axios = require('axios').default;
 // TODO CAMBIAR EL DOMINIO
-axios.defaults.baseURL = 'http://nixon.test/api';
+axios.defaults.baseURL = 'http://nixoncarpinteria.herokuapp.com/api';
+// axios.defaults.baseURL = 'http://nixoncarpinteria.herokuapp.com/api';
 
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
