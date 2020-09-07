85e1549299aa1c4e335728fa3073df6e9c77f4ee
Mario Moreno <mario_morenor@hotmail.com>
2020-09-07 16:41:06 -0500

    terminado

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -7,7 +7,7 @@
             <div class="carousel-inner">
               <div :class="{'carousel-item':true,'active':index==0?true:false  }"
                 v-for="(image,index) in product.images" :key="image.id">
-                <img :src="image.url" class="d-block  shadow-lg" id="imgPrincipal" alt="">
+                <img :src="image.url" class="d-block  w-100 shadow-lg" id="imgPrincipal" alt="">
               </div>
             </div>
             <a class="carousel-control-prev" href="#imagesCarousel" role="button" data-slide="prev">
@@ -93,7 +93,7 @@ data() {
   #imgPrincipal{
     border: 1px solid black;
     border-radius: 1rem;
-    width: 100%;
+   
     height: 25rem;
 }
 }
